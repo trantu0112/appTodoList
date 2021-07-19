442607ebbb8048652c7980118ccc0cf33160f02a
AnhTu <trantu01121999@gmail.com>
2021-07-19 21:55:27 +0700

    edit firebaseConnect.js

--- src/firebaseConnect.js
+++ src/firebaseConnect.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyCHCm3Gb9pOtAh83BcKcwetRV0NOoxhNxM",
-    authDomain: "notemanagerreact-330e6.firebaseapp.com",
-    databaseURL: "https://notemanagerreact-330e6-default-rtdb.firebaseio.com",
-    projectId: "notemanagerreact-330e6",
-    storageBucket: "notemanagerreact-330e6.appspot.com",
-    messagingSenderId: "791578793518",
-    appId: "1:791578793518:web:c0e4df9fb70031a140f1bf",
-    measurementId: "G-G093C6YH6B"
+    apiKey: "AIzaSyAb0-sELAke6uRIZac7m6REEEgtoePpoHw",
+    authDomain: "todolist-ddb4f.firebaseapp.com",
+    databaseURL: "https://todolist-ddb4f-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "todolist-ddb4f",
+    storageBucket: "todolist-ddb4f.appspot.com",
+    messagingSenderId: "1046346300409",
+    appId: "1:1046346300409:web:2989885242224124aedb28",
+    measurementId: "G-8537Z7R14M"
 };
 firebase.initializeApp(firebaseConfig);
-export const noteData = firebase.database().ref('dataForNote');
+export const noteData = firebase.database().ref();
